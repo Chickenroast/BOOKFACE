012e9d72f7dbdf4b3caed6d2a46de7bc5864bdf9
Julia Michel T. <juliamicheltex@gmail.com>
2024-01-10 20:23:21 +0100

    save

--- BOOKFACE/src/Pages/Sign.tsx
+++ BOOKFACE/src/Pages/Sign.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+
 enum FormState {
   First,
   Second,
@@ -12,7 +13,7 @@ function Sign() {
     password: "",
     confirmPassword: "",
     tags: "",
-    profileImageType: "string", // 'string' or '"url'
+    profileImageType: "string",
     profileImage: "",
     name: "",
     bio: "",
@@ -53,117 +54,172 @@ function Sign() {
   };
 
   return (
-    <div className=" w-screen h-screen">
-      <img src="shape1.svg" className="w-full h-full absolute z-[-10] bg-beigel" />
-      <div className=" flex flex-col lg-mx-auto lg:items-center lg:justify-center">
+    <div className="w-screen h-screen">
+      {/* <img
+        src="shape1.svg"
+        className="w-full h-full z-[-10] absolute rotate-45 opacity-70"
+      /> */}
+      <div className="flex flex-col md:items-center md:justify-center">
         <h1 className="p-5 text-5xl lg:mt-5 font-quicksand">SIGNUP</h1>
-        <p className="p-5 pt-1 font-quicksand">
-          Welcome, please insert all your information for the first form.
-        </p>
       </div>
       {formState === FormState.First && (
-        <div className=" lg:mx-auto bg-beiged flex m-5 flex-col items-center justify-center rounded-lg max-w-96">
-          <form className=" p-5" onSubmit={handleSubmitFirstForm}>
-            {/* Render first form fields */}
-            <div className=" p-5">
-              <label className="text-lg block">
-                Email:
-                <input
-                  type="text"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  className="w-full bg-beigel rounded-lg mt-1 px-3 py-2 text-sm"
-                />
-              </label>
-
-              <label className="text-lg block mt-3">
-                Username:
-                <input
-                  type="text"
-                  name="username"
-                  value={formData.username}
-                  onChange={handleChange}
-                  className="w-full bg-beigel rounded-lg mt-1 px-3 py-2 text-sm"
-                />
-              </label>
-
-              <label className="text-lg block mt-3">
-                Password:
-                <div className="relative">
+        <div className="flex flex-col md:items-center md:justify-center ">
+          <p className="p-5 pt-1 font-quicksand">
+            Welcome, please insert all your information for the first form.
+          </p>
+          <div className="lg:mx-auto bg-beiged flex m-5 flex-col items-center justify-center rounded-lg ">
+            <form className=" p-5" onSubmit={handleSubmitFirstForm}>
+              <div className=" p-5">
+                <label className="text-lg block">
+                  Email:
                   <input
-                    type={showPassword ? "text" : "password"}
-                    name="password"
-                    value={formData.password}
+                    type="text"
+                    name="email"
+                    value={formData.email}
                     onChange={handleChange}
                     className="w-full bg-beigel rounded-lg mt-1 px-3 py-2 text-sm"
                   />
-                  <button
-                    type="button"
-                    className="absolute right-0 top-1/2 transform -translate-y-1/2 px-3"
-                    onClick={handleShowPassword}
-                  >
-                    {showPassword ? (
-                      <img src="show-04.svg" className="w-12 h-12" />
-                    ) : (
-                      <img src="hide-05.svg" className="w-12 h-12" />
-                    )}
-                  </button>
-                </div>
-              </label>
+                </label>
 
-              <label className="text-lg block mt-3">
-                Confirm Password:
-                <div className="relative">
+                <label className="text-lg block mt-3">
+                  Username:
                   <input
-                    type={showPassword ? "text" : "password"}
-                    name="confirmPassword"
-                    value={formData.confirmPassword}
+                    type="text"
+                    name="username"
+                    value={formData.username}
                     onChange={handleChange}
-                    className={`w-full rounded-lg mt-1 px-3 py-2 text-sm ${
-                      !passwordsMatch
-                        ? "border-red-500"
-                        : "border-slate-300 bg-beigel"
-                    }`}
+                    className="w-full bg-beigel rounded-lg mt-1 px-3 py-2 text-sm"
                   />
-                  <button
-                    type="button"
-                    className="absolute right-0 top-1/2 transform -translate-y-1/2 px-3"
-                    onClick={handleShowPassword}
-                  >
-                    {showPassword ? (
-                      <img src="show-04.svg" className="w-12 h-12" />
-                    ) : (
-                      <img src="hide-05.svg" className="w-12 h-12" />
-                    )}
-                  </button>
-                </div>
-              </label>
-              {!passwordsMatch && (
-                <p className="text-red-500 mt-1 text-sm">
-                  Passwords do not match.
-                </p>
-              )}
-              <button
-                style={{ fontFamily: "Quicksand, sans-serif" }}
-                type="submit"
-                className="bg-brownl hover:bg-brownd w-1/2 px-5 rounded-full text-white pt-1 pb-1 mt-3"
-              >
-                Next
-              </button>
-            </div>
-          </form>
+                </label>
+
+                <label className="text-lg block mt-3">
+                  Password:
+                  <div className="relative">
+                    <input
+                      type={showPassword ? "text" : "password"}
+                      name="password"
+                      value={formData.password}
+                      onChange={handleChange}
+                      className="w-full bg-beigel rounded-lg mt-1 px-3 py-2 text-sm"
+                    />
+                    <button
+                      type="button"
+                      className="absolute right-0 top-1/2 transform -translate-y-1/2 px-3"
+                      onClick={handleShowPassword}
+                    >
+                      {showPassword ? (
+                        <img src="show-04.svg" className="w-12 h-12" />
+                      ) : (
+                        <img src="hide-05.svg" className="w-12 h-12" />
+                      )}
+                    </button>
+                  </div>
+                </label>
+
+                <label className="text-lg block mt-3">
+                  Confirm Password:
+                  <div className="relative">
+                    <input
+                      type={showPassword ? "text" : "password"}
+                      name="confirmPassword"
+                      value={formData.confirmPassword}
+                      onChange={handleChange}
+                      className={`w-full rounded-lg mt-1 px-3 py-2 text-sm ${
+                        !passwordsMatch
+                          ? "border-red-500"
+                          : "border-slate-300 bg-beigel"
+                      }`}
+                    />
+                    <button
+                      type="button"
+                      className="absolute right-0 top-1/2 transform -translate-y-1/2 px-3"
+                      onClick={handleShowPassword}
+                    >
+                      {showPassword ? (
+                        <img src="show-04.svg" className="w-12 h-12" />
+                      ) : (
+                        <img src="hide-05.svg" className="w-12 h-12" />
+                      )}
+                    </button>
+                  </div>
+                </label>
+                {!passwordsMatch && (
+                  <p className="text-red-500 mt-1 text-sm">
+                    Passwords do not match.
+                  </p>
+                )}
+                <button
+                  style={{ fontFamily: "Quicksand, sans-serif" }}
+                  type="submit"
+                  className="bg-brownl hover:bg-brownd w-1/2 px-5 rounded-full text-white pt-1 pb-1 mt-3"
+                >
+                  Next
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       )}
 
       {formState === FormState.Second && (
-        <div>
-          <p>Now, please insert additional information for the second form.</p>
-          <form onSubmit={handleSubmitSecondForm}>
-            {/* Render second form fields */}
-            {/* ... */}
-            <button type="submit">Submit</button>
-          </form>
+        <div className="flex flex-col md:items-center md:justify-center">
+          <p className="p-5 pt-1 font-quicksand">
+            Now, please insert additional information for the second form.
+          </p>
+          <div className="lg:mx-auto bg-beiged flex m-5 flex-col items-center justify-center rounded-lg ">
+            <form className="p-5" onSubmit={handleSubmitSecondForm}>
+              <div className=" p-5">
+                <label className="text-lg block">
+                  Select your Tags:
+                  <input
+                    type="text"
+                    name="tags"
+                    value={formData.tags}
+                    onChange={handleChange}
+                    className="w-full bg-beigel rounded-lg mt-1 px-3 py-2 text-sm"
+                  />
+                </label>
+
+                <label className="text-lg block mt-3">
+                  Profil Image:
+                  <input
+                    type="text"
+                    name="profil image"
+                    value={formData.profileImage}
+                    onChange={handleChange}
+                    className="w-full bg-beigel rounded-lg mt-1 px-3 py-2 text-sm"
+                  />
+                </label>
+                <label className="text-lg block mt-3">
+                  Name:
+                  <input
+                    type="text"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    className="w-full bg-beigel rounded-lg mt-1 px-3 py-2 text-sm"
+                  />
+                </label>
+                <label className="text-lg block mt-3">
+                  Bio:
+                  <input
+                    type="text"
+                    name="bio"
+                    value={formData.bio}
+                    onChange={handleChange}
+                    className="w-full bg-beigel rounded-lg mt-1 px-3 py-2 text-sm"
+                  />
+                </label>
+                <button
+                  style={{ fontFamily: "Quicksand, sans-serif" }}
+                  type="submit"
+                  className="bg-brownl hover:bg-brownd w-1/2 px-5 rounded-full text-white pt-1 pb-1 mt-3"
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       )}
     </div>
